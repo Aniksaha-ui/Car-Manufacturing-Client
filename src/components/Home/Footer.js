b8965782c924dcc48c670838d74d13dd12163d1e
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-24 23:01:41 +0600

    footer section done

--- src/components/Home/Footer.js
+++ src/components/Home/Footer.js
@@ -1,10 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <h3>Hello Footer</h3>
-    </div>
+    <footer class="footer p-10 bg-neutral text-neutral-content">
+      <div>
+        <span class="footer-title">Services</span>
+        <Link to="#" class="link link-hover">
+          Branding
+        </Link>
+        <Link to="#" class="link link-hover">
+          Design
+        </Link>
+        <Link to="#" class="link link-hover">
+          Marketing
+        </Link>
+        <Link to="#" class="link link-hover">
+          Advertisement
+        </Link>
+      </div>
+      <div>
+        <span class="footer-title">Company</span>
+        <Link to="#" class="link link-hover">
+          About us
+        </Link>
+        <Link to="#" class="link link-hover">
+          Contact
+        </Link>
+        <Link to="#" class="link link-hover">
+          Jobs
+        </Link>
+        <Link to="#" class="link link-hover">
+          Press kit
+        </Link>
+      </div>
+      <div>
+        <span class="footer-title">Legal</span>
+        <Link to="#" class="link link-hover">
+          Terms of use
+        </Link>
+        <Link to="#" class="link link-hover">
+          Privacy policy
+        </Link>
+        <Link to="#" class="link link-hover">
+          Cookie policy
+        </Link>
+      </div>
+    </footer>
   );
 };
 
