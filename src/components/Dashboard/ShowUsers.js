63e4d870e55dcaef0728c04c496e9a0ec16482ad
Aniksaha-ui <sahaanik1045@gmail.com>
2022-05-25 10:59:11 +0600

    verify admin and make a person admin

--- src/components/Dashboard/ShowUsers.js
+++ src/components/Dashboard/ShowUsers.js
@@ -3,8 +3,26 @@ import { toast } from "react-toastify";
 
 const ShowUsers = ({ user, refetch }) => {
   const { email, role } = user;
+
   const makeAdmin = () => {
-    console.log("make admin");
+    fetch(`http://localhost:4000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to Make an admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Successfully made an admin`);
+        }
+      });
   };
   return (
     <tr>
